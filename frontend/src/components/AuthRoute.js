b32e0a8a641ec3504a102c64b41019d80602865c
Moin Abbas <moin.malik996@gmail.com>
2025-08-10 04:45:07 +0100

    added new frontend functionality

--- frontend/src/components/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "../services/auth";
-
-const AuthRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
-};
-
-export default AuthRoute;
